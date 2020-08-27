58ec10daaed126505595000555ed154840b4d264
chickenZeo <hhthanh.mta@gmail.com>
2020-08-27 09:30:02 +0700

    Them Bai 1

---
+++ ThucTapXamarin/ThucTapXamarin/Greeter.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.Xaml;
+
+namespace ThucTapXamarin
+{
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Greeter : ContentPage
+    {
+        public Greeter()
+        {
+            InitializeComponent();
+
+            //if(Device.RuntimePlatform == Device.Android)
+            //{
+            //    Padding = new Thickness(10, 20, 0, 0);
+            //}
+                
+        }
+
+    }
+}
