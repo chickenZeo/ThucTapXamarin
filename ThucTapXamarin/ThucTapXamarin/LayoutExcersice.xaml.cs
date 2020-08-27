989c6e32ad047a06c5f8bbe992d1c70be984f0c0
chickenZeo <hhthanh.mta@gmail.com>
2020-08-27 09:44:51 +0700

    Bai 3 StackLayout Exercise

---
+++ ThucTapXamarin/ThucTapXamarin/LayoutExcersice.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ThucTapXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LayoutExcersice : ContentPage
+    {
+        public LayoutExcersice()
+        {
+            InitializeComponent();
+        }
+
+        void OnBtnLoginClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login successfull", "OK");
+        }
+        void OnBtnRegisterClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Register", "Register successfull", "OK");
+        }
+    }
+}
