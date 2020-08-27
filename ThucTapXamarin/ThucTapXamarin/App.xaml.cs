9ef5d7c3bb937bfdb38998622268b2d86ff8d02b
chickenZeo <hhthanh.mta@gmail.com>
2020-08-27 10:18:43 +0700

    Them GridLayout Exercise 2

--- ThucTapXamarin/ThucTapXamarin/App.xaml.cs
+++ ThucTapXamarin/ThucTapXamarin/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ThucTapXamarin
         {
             InitializeComponent();
 
-            MainPage = new Greeter();
+            MainPage = new GridExercise2();
         }
 
         protected override void OnStart()
